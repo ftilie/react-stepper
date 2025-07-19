e51681e1e19997bd07c190f2cc443868f66ac30e
Florin, Tilie <ftilie@ea.com>
2025-07-19 13:39:01 +0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1 style={{ color: 'white' }}>React Stepper Component</h1>
+    </div>
+  )
+}
+
+export default App
